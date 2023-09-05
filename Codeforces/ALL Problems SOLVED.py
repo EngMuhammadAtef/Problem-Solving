9351aa3d6ec7787b936d64ac8800bf03f01b4690
Muhammad Atef <96551959+EngMuhammadAtef@users.noreply.github.com>
2023-09-05 18:17:47 +0300

    ALL Problems SOLVED.py

--- Codeforces/ALL Problems SOLVED.py
+++ Codeforces/ALL Problems SOLVED.py
@@ -4,9 +4,9 @@ n = int(input())
 dollar_bills = [100, 20, 10, 5, 1]
 mini_bills = 0
  
-for element in dollar_bills:
-    mini_bills += n // element
-    n %= element
+for bill in dollar_bills:
+    mini_bills += n // bill
+    n %= bill
  
 print(mini_bills)
 ################################################################################################################################
