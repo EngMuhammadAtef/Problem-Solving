f67b82638c2180577a04d14c9092914da227a5fe
Muhammad Atef <96551959+EngMuhammadAtef@users.noreply.github.com>
2023-09-10 18:53:16 +0300

    ALL Problems that solved.py

--- Codeforces/ALL Problems SOLVED.py
+++ Codeforces/ALL Problems SOLVED.py
@@ -576,6 +576,21 @@ for _ in range(int(input())):
         if s[i]!=s[i+1]: res = "YES"
     print(res)
 ################################################################################################################################
+# Polycarp Writes a String from Memory
+
+for _ in range(int(input())):
+    s = input()
+    h = set()
+    res = 0
+    for i in range(len(s)): # lollipops
+        if len(h) >= 3 and s[i] not in h:
+            res += 1
+            h = set()
+ 
+        h.add(s[i])
+ 
+    print(res if len(h)==0 else res+1)
+################################################################################################################################
 # Kuriyama Mirai's Stones
 def getResult(q,S,E):
     if q == 1:
